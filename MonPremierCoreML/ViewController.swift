1680a9b88442e8d4f19b34de7ad45c8898948ed4
Rodolphe DUPUY <rodolphe.dupuy@free.fr>
2020-10-02 07:43:10 +0200

    V1.1 - CoreML

--- MonPremierCoreML/ViewController.swift
+++ MonPremierCoreML/ViewController.swift
@@ -16,32 +16,48 @@ class ViewController: UIViewController {
     @IBOutlet weak var launchML: UIButton!
     
     var picker = UIImagePickerController()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         launchML.isEnabled = false
+        
         picker.delegate = self
         picker.allowsEditing = false
     }
     
+    @IBAction func takeAPic(_ sender: UIButton) {
+        setupCamera()
+    }
+    
+    
+    // MARK: CoreML
+    // 1. Convertie en cgImage
+    // 2. Obtenir mon modele CoreML avec un try
+    // 3. Convertir mlmodel en visonModel avec un try
+    // 4. Faire request
+    // 5. Handler
+    // 6. Faire ma requete
+    // 7.Annalyser ce que l'on recoit
     @IBAction func coreMLAction(_ sender: Any) {
         // 1. Convertie en cgImage
         if let imageToParse = iv.image?.cgImage {
             do {
-                //2. Obtenir mon modele CoreML
+                // 2. Obtenir mon modele CoreML avec un try
                 let model = try SqueezeNet(configuration: MLModelConfiguration())
                 let mlModel = model.model
                 do {
-                    //3. Convertir mlmodel en visonModel
+                    // 3. Convertir mlmodel en visonModel avec un try
                     let visionModel = try VNCoreMLModel(for: mlModel)
-                    //4. Faire request
+                    // 4. Faire request
                     let request = VNCoreMLRequest(model: visionModel) { (response, error) in
-                        //7.Annalyser ce que l'on recoit
+                        // 7.Annalyser ce que l'on recoit
                         if let e = error {
                             print("Erreur chef: \(e.localizedDescription)")
                         }
                         if let r = response.results as? [VNClassificationObservation] {
-                            if let f = r.first {
+                            print(r)
+                            if let f = r.first { // On prend la 1ere ligne
                                 let id = f.identifier
                                 let confidenceToPercent = f.confidence * 100
                                 let confidenceString = String(format: "%.2f", confidenceToPercent)
@@ -52,9 +68,9 @@ class ViewController: UIViewController {
                             }
                         }
                     }
-                    //5. Handler
+                    // 5. Handler
                     let handler = VNImageRequestHandler(cgImage: imageToParse, options: [:])
-                    //6. Faire ma requete
+                    // 6. Faire ma requete
                     try handler.perform([request])
                 } catch {
                     print(error.localizedDescription)
@@ -65,11 +81,10 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func takeAPic(_ sender: UIButton) {
-        setupCamera()
-    }
-    
+    // MARK: Config Appareil photo
     func setupCamera() {
+        // Ajouter dans plist: Privacy - Photo Library Usage Description
+        // Ajouter dans plist: Privacy - Camera Usage Description
         let controller = UIAlertController(title: "Choisir une image", message: "Photo ou gallerie?", preferredStyle: .alert)
         let photo = UIAlertAction(title: "Appareil Photo", style: .default) { (action) in
             self.picker.sourceType = .camera
